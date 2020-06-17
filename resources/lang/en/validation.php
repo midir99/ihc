c4ec764a5bddb88ac5a4cdedef11ff80fc25be6b
parzival400 <giliux@live.com.mx>
2020-06-17 15:53:37 -0500

    Iteración 1

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -85,9 +85,9 @@ return [
     'mimes' => 'The :attribute must be a file of type: :values.',
     'mimetypes' => 'The :attribute must be a file of type: :values.',
     'min' => [
-        'numeric' => 'The :attribute must be at least :min.',
+        'numeric' => 'El campo :attribute debe tener al menos :min.',
         'file' => 'The :attribute must be at least :min kilobytes.',
-        'string' => 'The :attribute must be at least :min characters.',
+        'string' => 'El campo :attribute debe tener al menos :min caracteres.',
         'array' => 'The :attribute must have at least :min items.',
     ],
     'not_in' => 'The selected :attribute is invalid.',
